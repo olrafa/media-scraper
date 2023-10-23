7dc545b0bc22ef47c28720e998fcaaad5a8ad894
Rafael Olivares <olrafael@mailfence.com>
2023-10-23 11:05:16 -0300

    Setup scraper

---
+++ constants.mjs
@@ -0,0 +1,18 @@
+export const SEARCH_TERMS = ["Neymar"];
+
+export const WEBSITES = [
+  { mediaOutlet: "UOL", url: "http://www.uol.com.br" },
+  { mediaOutlet: "Folha de S. Paulo", url: "https://www.folha.uol.com.br/" },
+  { mediaOutlet: "Estado de S. Paulo", url: "https://www.estadao.com.br/" },
+  { mediaOutlet: "Globo", url: "https://www.globo.com/" },
+  { mediaOutlet: "G1", url: "https://g1.globo.com/" },
+  { mediaOutlet: "O Globo", url: "https://oglobo.globo.com/" },
+  { mediaOutlet: "R7", url: "https://www.r7.com/" },
+  { mediaOutlet: "Jornal do Brasil", url: "https://www.jb.com.br/" },
+  { mediaOutlet: "Globo Esporte", url: "https://globoesporte.globo.com/" },
+  { mediaOutlet: "ESPN", url: "https://www.espn.com.br/" },
+  { mediaOutlet: "SporTV", url: "https://sportv.globo.com/" },
+  { mediaOutlet: "TNT Sports", url: "https://tntsports.com.br/" },
+  { mediaOutlet: "CNN Brasil", url: "https://www.cnnbrasil.com.br/" },
+  { mediaOutlet: "Veja", url: "https://veja.abril.com.br/" },
+];
